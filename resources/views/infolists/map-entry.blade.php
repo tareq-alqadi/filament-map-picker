205bc6fdc25ccff297587c7a27e840b170d3c88f
Tareq Al-Qadi <alqadi.tareq199@gmail.com>
2025-10-24 21:51:08 +0300

    fix async alpine directives

--- resources/views/infolists/map-entry.blade.php
+++ resources/views/infolists/map-entry.blade.php
@@ -8,16 +8,19 @@
 
     @if (filled($state))
         <div x-data="mapPicker({
-            id: @js($id),
-            state: @js($state),
-            config: @js($config),
-            toolbarButtons: @js($getToolbarButtons()),
-            hasMarkerCircle: @js($hasMarkerCircle()),
-        })" x-ignore ax-load wire:ignore
+                id: @js($id),
+                state: @js($state),
+                config: @js($config),
+                toolbarButtons: @js($getToolbarButtons()),
+                hasMarkerCircle: @js($hasMarkerCircle()),
+            })"
+            x-ignore
+            wire:ignore
+            x-load="visible"
             x-load-css="[
                 @js(\Filament\Support\Facades\FilamentAsset::getStyleHref('filament-map-picker-css', \TareqAlqadi\FilamentMapPicker\FilamentMapPicker::getPackageName())),
             ]"
-            ax-load-src="{{ \Filament\Support\Facades\FilamentAsset::getAlpineComponentSrc('map-picker-component', package: \TareqAlqadi\FilamentMapPicker\FilamentMapPicker::getPackageName()) }}">
+            x-load-src="{{ \Filament\Support\Facades\FilamentAsset::getAlpineComponentSrc('map-picker-component', package: \TareqAlqadi\FilamentMapPicker\FilamentMapPicker::getPackageName()) }}">
 
             <div x-ref="map-{{ $id }}" class="w-full border-2 dark:border-gray-800 rounded-lg"
                 style="height: {{ $config['height'] }}px; z-index: 1 !important;"></div>
